1e4af3dfc013e5d858a5815cdc3c7a71d00d6e58
Mehmet Ozgul <91568457+mehmetozguldev@users.noreply.github.com>
2024-09-21 15:03:46 +0300

    feat: authenticator class

---
+++ Http/controllers/session/store.php
@@ -0,0 +1,20 @@
+<?php
+
+use Core\Authenticator;
+use Http\Forms\LoginForm;
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$form = new LoginForm();
+
+if (!$form->validate($email, $password)) {
+    if ((new Authenticator)->attempt($email, $password)) {
+        redirect('/');
+    }
+    $form->error('email', 'No matching account found for that email address and password.');
+}
+
+return view('sessions/create.view.php', [
+    'errors' => $form->errors()
+]);
